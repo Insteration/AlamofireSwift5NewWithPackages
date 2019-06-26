1b378fd4c2c4188f45bd2b00dfef5d701700c351
Artem <inste@icloud.com>
2019-06-26 19:58:00 +0300

    Initial Commit

---
+++ AlamofireSwift5NewWithPackages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireSwift5NewWithPackages
+//
+//  Created by Артём Кармазь on 6/26/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
