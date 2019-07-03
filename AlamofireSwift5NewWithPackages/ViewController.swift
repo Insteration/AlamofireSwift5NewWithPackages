e51a13b12a7a31ce62815915badaf2e739ebec53
Artem <inste@icloud.com>
2019-07-03 20:28:46 +0300

    NC2

--- AlamofireSwift5NewWithPackages/ViewController.swift
+++ AlamofireSwift5NewWithPackages/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     let parser = String()
     let parser2 = String()
     let parser3 = String()
+    let parser4 = String()
     
     private func getResult() -> DataRequest {
         
